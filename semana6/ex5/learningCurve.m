2cef70c227949b325a5f9f0c3a83c27df096bf14
yury <yuryjefse@gmail.com>
2020-07-13 22:39:16 -0300

    finalizando o exerc 5

--- semana6/ex5/learningCurve.m
+++ semana6/ex5/learningCurve.m
@@ -54,10 +54,15 @@ error_val   = zeros(m, 1);
 % ---------------------- Sample Solution ----------------------
 
 
+for i=1:m
+	theta = trainLinearReg(X(1:i,:),y(1:i,:),lambda);
+	[jt, g] = linearRegCostFunction(X(1:i,:),y(1:i,:),theta,0);
+	[jv, g] = linearRegCostFunction(Xval,yval,theta,0);
+	
+	error_train(i) = jt;
+	error_val(i)  = jv;
 
-
-
-
+endfor
 
 % -------------------------------------------------------------
 
